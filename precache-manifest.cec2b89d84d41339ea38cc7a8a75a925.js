e99bec753553176341ca078780ed5ae9679018ad
Win Lam <winlam@Mormont.local>
2020-02-10 17:19:17 -0800

    Initial dist subtree commit

---
+++ precache-manifest.cec2b89d84d41339ea38cc7a8a75a925.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "192a6b7306cc3c1b70958a1612113d1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfb062a669140941cc4a",
+    "url": "/static/css/main.e53989c5.chunk.css"
+  },
+  {
+    "revision": "c8c9959c4a9a17aa2173",
+    "url": "/static/js/2.72168356.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.72168356.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bfb062a669140941cc4a",
+    "url": "/static/js/main.6676d6d1.chunk.js"
+  },
+  {
+    "revision": "6c7a709f7c5b4ec77f08",
+    "url": "/static/js/runtime-main.d2d1c915.js"
+  },
+  {
+    "revision": "393300cc61841bfc24a9e26498ec7e98",
+    "url": "/static/media/books.393300cc.png"
+  }
+]);
